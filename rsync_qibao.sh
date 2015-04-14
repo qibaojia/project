38284467046cdc94beed1fb06efa882b01d996e9
jiayahong <jiayahong@55tuan.com>
2015-04-14 17:55:16 +0800

    同步脚本修复

--- rsync_qibao.sh
+++ rsync_qibao.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-rsync -rtu --delete ./ --exclude ".git" --exclude ".idea"   --progress root@qibaojia.com::/usr/local/nginx/html/www
+rsync -rtu --delete ./ --exclude ".git" --exclude ".idea"   --progress root@qibaojia.com:/usr/local/nginx/html/www
